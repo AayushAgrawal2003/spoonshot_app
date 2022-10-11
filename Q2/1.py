42db09311f5bd8ffba2e0eb4084dadde8e4772b8
AayushAgrawal2003 <aayush.agrawal2003@gmail.com>
2022-10-12 01:12:05 +0530

    user input added

--- Q2/1.py
+++ Q2/1.py
@@ -1,12 +1,16 @@
-nums = [5, 1, 4, 2]
-
+print("Enter numbers in series with spaces in the middle:")
+nums = input().split()
+nums = list(map(int, nums))
 output = [1] * len(nums)
 
+#forward traversing - multiplying elements before the element
 pre = 1
 for i in range(len(nums)):
     output[i] = pre
     pre *= nums[i]
 
+
+#backward traversing - multiplying elements after the element
 post = 1
 for i in reversed(range(len(nums))):
     output[i] *= post
--- Q2/2.py
+++ Q2/2.py
@@ -1,8 +1,11 @@
-arr = [0,1,1,2,12]
+print("Enter numbers in series with spaces in the middle:")
+arr = input().split()
+arr = list(map(int, arr))
 flag = 0
 store = []
 final = []
 product = 1 
+#counting 0's and calculating product 
 for i in range(len(arr)):
     if arr[i] == 0:
         flag += 1
@@ -11,9 +14,11 @@ for i in range(len(arr)):
         product *= arr[i]
 
 for i in range(len(arr)):
+    #checking for multiple 0's
     if (flag > 1):
         final.append(0)
         continue
+    #checking for single 0
     elif (flag == 1):
         if (arr[i] == 0):
             final.append(product)
